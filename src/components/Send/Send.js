7c3703022c177a6d946102830ad1262b6c17c94f
Jojjerg <filet814@gmail.com>
2022-12-13 23:46:07 +0700

    добавлена функционал отравки токена

--- src/components/Send/Send.js
+++ src/components/Send/Send.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useContext, useState, memo } from "react";
-import { AiOutlineDown } from "react-icons/ai";
 import Modal from "react-modal";
-import transferToken from "./transferTokens";
-import { ImCross } from "react-icons/im";
 import { UserContext } from "../../context/UserContext";
+import { TransactionContext } from "../../context/TransactionContext";
 import { useTranslation } from "react-i18next";
+
+import { ColorRing } from "react-loader-spinner";
+
+import bnbIcon from "../../assets/icons/binanceSmartChain.png";
 
 Modal.setAppElement(document.getElementById("root"));
 
@@ -32,143 +34,49 @@ const styles = {
   transferPropInput: `
     bg-[#ededed] p-3 rounded-full placeholder:text-[#abc0c2] outline-none w-full text-2xl
   `,
+  currencySelectorContainer: `flex flex-row justify-between`,
   currencySelector: `
-    flex flex-row w-max
+    flex flex-row items-center px-4 gap-2 w-max rounded-full bg-[#ededed] font-semibold
     phone:w-max
   `,
-  currencySelectorContent: `
-    w-full h-min justify-between flex items-center shadow shadow-lg hover:shadow-none transtition-all duration-100 rounded-3xl text-[#373C3D] font-medium cursor-pointer p-2 
-  `,
-  currencySelectorTicker: `
-    flex flex-row items-center ml-2 font-semibold
-  `,
-  currencySelectorArrow: `
-    mr-2 ml-4 text-md
-  `,
   icon: `
-    pt-1 pr-2
-  `,
-  addIcon: `
-    flex flex-row items-center justify-center p-2 rounded-full
+    w-[20px] h-[20px]
   `,
   confirmButton: `
-    w-full bg-[#049CA6] my-2 rounded-full py-5 px-8 text-xl text-white flex items-center justify-center cursor-pointer shadow shadow-xl hover:shadow-none hover:bg-[#05a4ad] transtition-all duration-100 disabled:bg-[#d9d9d9] disabled:text-[#373C3D] disabled:border-[#d9d9d9] disabled:shadow-none
-  `,
-
-  modalWrapper: `
-    p-4 bg-[#E6E6E6] text-[#373C3D] h-min w-[350px] flex flex-col justify-evenly rounded-2xl
-    phone:w-[325px]
-  `,
-  titleWrapper: `flex flex-row p-2 justify-between items-center`,
-  title: `
-    font-semibold text-xl 
-    phone:text-lg
-  `,
-  searchBar: `flex mt-4 justify-center items-center placeholder:text-[#373C3D] w-full`,
-  searchPropInput: `bg-[#ededed] p-3 rounded-full placeholder:text-[#abc0c2] outline-none mb-6 w-full text-md`,
-  tokenButton: `px-1 py-2 my-1 flex flex-row items-center bg-[#E6E6E6] text-lg font-semibold text-[#373C3D] cursor-pointer rounded-full hover:bg-[#dadada] transtition-all duration-100`,
-  exitButton: `text-[#373C3D] rounded-full flex justify-center items-center hover:rotate-90 transition-all duration-200`,
+    w-full bg-[#049CA6] my-2 rounded-full py-5 px-8 text-xl text-white flex items-center justify-center cursor-pointer shadow shadow-xl hover:shadow-none hover:bg-[#05a4ad] transtition-all duration-100 disabled:bg-[#d9d9d9] disabled:text-[#373C3D] disabled:border-[#d9d9d9] disabled:shadow-none focus:border-none
+  `
 };
-
-const customstyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    transform: "translate(-50%, -50%)",
-    backgroundColor: "transparent",
-    padding: 0,
-    border: "none",
-    zIndex: 100,
-  },
-  overlay: {
-    backgroundColor: "rgba(10, 11, 13, 0.3)",
-    backdropFilter: "blur(2px)",
-    zIndex: 100,
-  },
-};
-
-const Sending = () => {
-
-}
 
 const Send = memo(() => {
   const { txhash, getWeiBalance, walletBalance } = useContext(UserContext);
+  const {
+    handleChange,
+    showError,
+    sendTransaction,
+    isTransactionLoading,
+    formData,
+  } = useContext(TransactionContext);
 
-  const [isShowing, setIsShowing] = useState();
-  const [formData, setFormData] = useState({
-    receiver: "",
-    amount: "0.0",
-  });
-
-  const [showError, setShowError] = useState(false);
-
-  const [filter, setFilter] = useState("");
-
-  const [token, setToken] = useState({
+  const [token] = useState({
     symbol: "BNB",
-    icon: "binanceSmartChain",
+    icon: bnbIcon,
     contractAddress: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
     decimals: 18,
   });
 
-  const selectTransferToken = (symbol, icon, address, decimals) => {
-    setToken({
-      symbol: symbol,
-      icon: icon,
-      contractAddress: address,
-      decimals: decimals,
-    });
+  const handleSubmit = async (e) => {
+    let { addressTo, amount } = formData;
+    e.preventDefault();
 
-    setIsShowing(false);
-    setFilter("");
+    if (!addressTo || !amount) return;
+
+    sendTransaction().then((e.target.value = ""));
   };
-
-  const handleChange = (e, name) => {
-    const str = e.target.value;
-
-    setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
-
-    if (str.length) {
-      const re = /^\d+\.{0,1}\d*$/g;
-      const address = /^0x[a-fA-F0-9]{40}$/g;
-
-      if (
-        re.test(String(str).toLowerCase()) ||
-        address.test(String(str).toLowerCase())
-      ) {
-        return setShowError(false);
-      }
-      return setShowError(true);
-    }
-    return setShowError(false);
-  };
-
-  const filterItems = (filter) => {
-    return filter
-      ? transferToken.filter((t) => t.title.includes(filter))
-      : transferToken;
-  };
-
-  const items = filterItems(filter.toUpperCase());
-
-  const searchHandler = (e) => {
-    setFilter(e.target.value);
-  };
-
-  const closeModal = () => {
-    setFilter("");
-    setIsShowing(false);
-  };
-
-  useEffect(() => {
-    console.log(token);
-  }, [token]);
 
   useEffect(() => {
     getWeiBalance(txhash);
   }, [getWeiBalance, txhash]);
+
   const { t } = useTranslation();
 
   return (
@@ -179,40 +87,21 @@ const Send = memo(() => {
             <div>{t("send.sending")}</div>
           </div>
 
-          <div className={styles.addIcon}></div>
-
           <div className={styles.transferPropContainer}>
             <input
               type="text"
               className={styles.transferPropInput}
               placeholder="0.0"
-              pattern="^[0-9]*[.,]?[0-9]*$"
               onChange={(e) => handleChange(e, "amount")}
             />
-            <div className="flex flex-row justify-between">
-              <div
-                onClick={() => setIsShowing(true)}
-                className={styles.currencySelector}
-              >
-                <div className={styles.currencySelectorContent}>
-                  <div className={styles.currencySelectorTicker}>
-                    <div className={styles.icon}>
-                      <img
-                        width={20}
-                        height={20}
-                        src={require("../../assets/icons/" +
-                          token.icon +
-                          ".png")}
-                        alt=""
-                      />
-                    </div>
-                    <div>{token.symbol}</div>
-                  </div>
-                  <AiOutlineDown className={styles.currencySelectorArrow} />
-                </div>
+            <div className={styles.currencySelectorContainer}>
+              <div className={styles.currencySelector}>
+                <img className={styles.icon} src={token.icon} alt="" />
+                <span>{token.symbol}</span>
               </div>
               <span className="p-[10px] font-semibold">
-                {t("swap.balance")}: {walletBalance?.substring(0, 6)}
+                {t("swap.balance")}:{" "}
+                {Math.round(parseFloat(walletBalance) * 1000) / 1000}
               </span>
             </div>
           </div>
@@ -221,11 +110,15 @@ const Send = memo(() => {
               type="text"
               className={styles.transferPropInput}
               placeholder="0x..."
-              onChange={(e) => handleChange(e, "receiver")}
+              onChange={(e) => handleChange(e, "addressTo")}
             />
           </div>
           {walletBalance > 0 ? (
-            <button disabled={showError} className={styles.confirmButton}>
+            <button
+              disabled={showError || isTransactionLoading}
+              className={styles.confirmButton}
+              onClick={(e) => handleSubmit(e)}
+            >
               {t("send.sendConfirm")}
             </button>
           ) : (
@@ -234,52 +127,6 @@ const Send = memo(() => {
             </button>
           )}
         </div>
-        <Modal isOpen={!!isShowing} style={customstyles}>
-          <div className={styles.modalWrapper}>
-            <div className={styles.titleWrapper}>
-              <div className={styles.title}>{t("send.sendChooseToken")}</div>
-              <div onClick={() => closeModal()} className={styles.exitButton}>
-                <ImCross />
-              </div>
-            </div>
-            <div className={styles.searchBar}>
-              <input
-                type="text"
-                className={styles.searchPropInput}
-                placeholder="Введите название токена"
-                onChange={(e) => {
-                  searchHandler(e);
-                }}
-              />
-            </div>
-            <div className="item-center mt-5 flex h-[380px] flex-col justify-start overflow-hidden scroll-smooth p-2">
-              {items?.map((t) => (
-                <button
-                  onClick={() =>
-                    selectTransferToken(
-                      t.symbol,
-                      t.icon,
-                      t.contractAddress,
-                      t.decimals
-                    )
-                  }
-                  key={t.id}
-                  className={styles.tokenButton}
-                >
-                  <div className="pl-2 pr-5">
-                    <img
-                      width={38}
-                      height={38}
-                      src={require("../../assets/icons/" + t.icon + ".png")}
-                      alt=""
-                    />
-                  </div>
-                  <div>{t.symbol}</div>
-                </button>
-              ))}
-            </div>
-          </div>
-        </Modal>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,48 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {createClient,Provider,subscriptionExchange, defaultExchanges} from "urql";
-import { createClient as createWSClient } from "graphql-ws";
 import './i18next';
+import "./index.css";
 
 import { UserProvider } from "./context/UserContext";
 import { TokenProvider } from "./context/TokenContext";
+import { TransactionProvider } from "./context/TransactionContext";
 
 function Main() {
   return (
     <React.StrictMode>
       <UserProvider>
       <TokenProvider>
-        <Provider value={client}>
-          
-          <App />
-          
-        </Provider>
+      <TransactionProvider>   
+        <App />
+      </TransactionProvider>  
       </TokenProvider>
       </UserProvider>
     </React.StrictMode>
   );
 }
-
-const wsClient = createWSClient({
-  url: "ws://localhost:7000/graphql",
-});
-
-const client = createClient({
-  url: "http://localhost:7000/graphql",
-  exchanges: [
-    ...defaultExchanges,
-    subscriptionExchange({
-      forwardSubscription: (operation) => ({
-        subscribe: (sink) => ({
-          unsubscribe: wsClient.subscribe(operation, sink),
-        }),
-      }),
-    }),
-  ],
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Main />);
